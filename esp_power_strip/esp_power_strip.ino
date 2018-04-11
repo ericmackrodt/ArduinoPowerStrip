e81e5594c517add191f7498c20772a979e607ba7
Eric Mackrodt <ericmackrodt@Loaners-MacBook-Pro.local>
2018-04-11 23:55:26 +1000

    Add wifi configuration portal.

--- esp_power_strip/esp_power_strip.ino
+++ esp_power_strip/esp_power_strip.ino
@@ -1,60 +1,47 @@
 #include <ESP8266WiFi.h>
-#include <WiFiClient.h>
+//#include <WiFiClient.h>
+#include <DNSServer.h>
 #include <ESP8266WebServer.h>
-#include <ESP8266mDNS.h>
+//#include <ESP8266mDNS.h>
 #include <SerialMessenger.h>
-
-const char *ssid = "Luminol";
-const char *password = "spritelynevada666";
-
-ESP8266WebServer server ( 80 );
+#include <WiFiManager.h>
 SerialMessenger messenger;
 
-void handleStatus() {
-  messenger.send("status");
-}
-
-void handleOn() {
-//  serial_manager.print("status;"); 
-  if (server.argName(0) == "relay") {
-    messenger.send("on", server.arg(0));
-  }
-}
-
-void handleOff() {
-//  serial_manager.print("status;"); 
-  if (server.argName(0) == "relay") {
-    messenger.send("off", server.arg(0));
-  }
-}
+std::unique_ptr<ESP8266WebServer> server;
 
 void handleNotFound() {
 	String message = "File Not Found\n\n";
 	message += "URI: ";
-	message += server.uri();
+	message += server->uri();
 	message += "\nMethod: ";
-	message += ( server.method() == HTTP_GET ) ? "GET" : "POST";
+	message += ( server->method() == HTTP_GET ) ? "GET" : "POST";
 	message += "\nArguments: ";
-	message += server.args();
+	message += server->args();
 	message += "\n";
 
-	for ( uint8_t i = 0; i < server.args(); i++ ) {
-		message += " " + server.argName ( i ) + ": " + server.arg ( i ) + "\n";
+	for ( uint8_t i = 0; i < server->args(); i++ ) {
+		message += " " + server->argName ( i ) + ": " + server->arg ( i ) + "\n";
 	}
 
-	server.send ( 404, "text/plain", message );
+	server->send ( 404, "text/plain", message );
 }
 
 void onStatus(String message) {
-  server.send ( 200, "text/json", message ); 
+  server->send ( 200, "text/json", message ); 
 }
 
 void onTurnOnResponse(String message) {
-  server.send ( 200, "text/json", message ); 
+  server->send ( 200, "text/json", message ); 
 }
 
 void onTurnOffResponse(String message) {
-  server.send ( 200, "text/json", message ); 
+  server->send ( 200, "text/json", message ); 
+}
+
+void resetWifi(String message) {
+  WiFiManager wifiManager;
+  wifiManager.resetSettings();
+  ESP.restart();
 }
 
 void setup ( void ) {
@@ -63,14 +50,19 @@ void setup ( void ) {
   messenger.subscribe("on", &onTurnOnResponse);
   messenger.subscribe("off", &onTurnOffResponse);
 
-	WiFi.mode ( WIFI_STA );
-	WiFi.begin ( ssid, password );
+  messenger.send("connecting");
+  WiFiManager wifiManager;
+  wifiManager.autoConnect("EricPowerStrip");
+  
+
+//	WiFi.mode ( WIFI_STA );
+//	WiFi.begin ( ssid, password );
 //	Serial.println ( "" );
 
 	// Wait for connection
-	while ( WiFi.status() != WL_CONNECTED ) {
-		delay ( 500 );
-	}
+//	while ( WiFi.status() != WL_CONNECTED ) {
+//		delay ( 500 );
+//	}
 
 //	Serial.println ( "" );
 //	Serial.print ( "Connected to " );
@@ -78,22 +70,29 @@ void setup ( void ) {
 //	Serial.print ( "IP address: " );
 //	Serial.println ( WiFi.localIP() );
 
-	if ( MDNS.begin ( "esp8266" ) ) {
+//	if ( MDNS.begin ( "esp8266" ) ) {
 //		Serial.println ( "MDNS responder started" );
-	}
+//	}
 
-	server.on ( "/status", handleStatus );
-  server.on ( "/on", handleOn );
-  server.on ( "/off", handleOff );
-	server.on ( "/inline", []() {
-		server.send ( 200, "text/plain", "this works as well" );
-	} );
-	server.onNotFound ( handleNotFound );
-	server.begin();
-	Serial.println ( "HTTP server started" );
+  server.reset(new ESP8266WebServer(WiFi.localIP(), 80));
+
+	server->on ( "/status", []() { messenger.send("status"); } );
+  server->on ( "/on", []() {
+    if (server->argName(0) == "relay") {
+      messenger.send("on", server->arg(0));
+    }
+  } );
+  server->on ( "/off", []() {
+    if (server->argName(0) == "relay") {
+      messenger.send("off", server->arg(0));
+    }
+  } );
+	server->onNotFound ( handleNotFound );
+	server->begin();
+	messenger.send("connected", String(WiFi.localIP()));
 }
 
 void loop ( void ) {
-  server.handleClient();
+  server->handleClient();
   messenger.handle();
 }
